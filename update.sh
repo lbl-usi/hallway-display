6f55ae0d84f04774f24f0d550947f6b1aec7b362
Sheng Pang <shengdevelop@gmail.com>
2023-10-31 11:16:12 -0700

    update git pull repo display computer won't do push so use http pull from public repo

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 while true
-do
-    git pull ssh://git@bitbucket.org/yocheah/dst-showcase-repo.git
+do  
+    # old repo reference
+    # git pull ssh://git@bitbucket.org/yocheah/dst-showcase-repo.git
+
+    # only pull from public repo.
+    git pull https://github.com/lbl-usi/hallway-display.git
     sleep 150
 done
 
